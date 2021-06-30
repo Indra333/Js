37b92c5ce1686f8e928164ca6bc35534c025048c
DESKTOP-IMR9BGF\indrasena reddy <annapureddyindra333@gmail.com>
2021-06-30 22:21:31 +0530

    warning changes

--- functions/challenge3.js
+++ functions/challenge3.js
@@ -2,8 +2,8 @@ function outerFunction(a,b){
     function innerFunction(c){
         return c*c;
     }
-    let c=a+b;
-    console.log(innerFunction(c));
+    let d=a+b;
+    console.log(innerFunction(d));
 }
 
 outerFunction(2,3);
