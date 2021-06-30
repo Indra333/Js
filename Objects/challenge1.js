a92ed61fd93d7847145956aabebf693aa8c9cc29
DESKTOP-IMR9BGF\indrasena reddy <annapureddyindra333@gmail.com>
2021-06-30 12:05:41 +0530

    challenge1

---
+++ Objects/challenge1.js
@@ -0,0 +1,17 @@
+var myPost={};
+console.log(myPost);
+
+myPost.postTitle="Object is reference type";
+console.log(myPost);
+
+myPost.postLikes=0;
+console.log(myPost);
+
+myPost.shared=false;
+console.log(myPost);
+
+myPost.postLikes=myPost.postLikes+1;
+console.log(myPost);
+
+delete myPost.shared
+console.log(myPost);
