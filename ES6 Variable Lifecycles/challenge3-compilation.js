629ecf8b540138bedf6be2535637ffa4e922a90e
DESKTOP-IMR9BGF\indrasena reddy <annapureddyindra333@gmail.com>
2021-07-07 15:38:09 +0530

    challenges

---
+++ ES6 Variable Lifecycles/challenge3-compilation.js
@@ -0,0 +1,14 @@
+/*
+global{
+line 16: b is declared but not initialized
+
+line 24: a is declared and initializes as 'undefined'
+
+line 26: c is declared and initializes as 'undefined'
+}
+
+
+block{
+    line 21: c is declared but not initialized
+}
+*/
---
+++ ES6 Variable Lifecycles/challenge3-execution.js
@@ -0,0 +1,18 @@
+/*
+global{
+line 16: b is assigned with value 5 
+
+line 18: a is assigned with value b i.e., 5
+
+line 24: a is assigned with value 1
+
+line 26: c is not assigned. so value remains 'undefined'
+}
+
+
+ block{
+    line 21: c is assigned with value 1
+}
+
+ line 28: console logs a=1, b=5, c=undefined
+*/
